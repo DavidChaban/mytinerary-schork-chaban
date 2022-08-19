d8a15524c1e49df50335ec02ba6513a435b11226
Mauro Schork <mauroschork107@hotmail.com>
2022-08-18 23:00:11 -0300

    modifique el js de WelcomePage

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -6,13 +6,7 @@ function WelcomePage() {
     return (
             <div className='App-container'>
                 <h1 className='App-tittle'>MyTinerary</h1>
-                <h2 className='App-slogan'>Dream big, we will make it possible</h2>
-                <h3 className='App-tittle2'>About Us</h3>
-                <p className='App-text'>We are a page designed so that you can fully enjoy your next trip, without worrying about lodging, expeditions, logistics, etc.</p>
-                <p className='App-text'>We are backed by more than 10 years of experience and more than excellent reviews. so we can assure you guarantee and security when trusting us
-                </p>
-                <p className='App-text'>We offer multiple options when choosing your destination. We take into account the objective of your trip, your budget, the people who travel, their tastes and we are open to new suggestions in order to improve our service.
-                </p>
+                <h2 className='App-slogan'>Find your perfect trip, designed by insiders who know and love their cities!</h2>
                 <button className='App-button'>Enter the site</button>
             </div>
     );
